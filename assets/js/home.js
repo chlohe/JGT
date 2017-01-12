6820ed3dad2f1d23e2e21044a50b7bb91b1ab32b
Peter <prandomguy@gmail.com>
2017-01-12 07:21:47 +0000

    Lesser compendium stuff

--- assets/js/home.js
+++ assets/js/home.js
@@ -17,32 +17,3 @@ function fixFooter(){
     $("#footer-home .icons").css("margin","0.5rem");
 
 }
-
-$("a").click(function(){
-
-    var target = $(this).data("target-id");
-    var current = $(this).data("current-id");
-
-    if (target && current){
-
-        //Mad transition tings - fade out and in
-
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        $("#"+current).addClass("animated fadeOut").one(animationEnd, function() {
-            $("#"+current).addClass("hidden");
-            $("#"+current).removeClass("animated fadeOut");
-            $("#"+target).removeClass("hidden");
-            $("#"+target).addClass("visible");
-            $("#"+target).addClass("animated fadeIn visible").one(animationEnd, function() {
-                 $("#"+target).removeClass("animated fadeIn visible");
-                 fixFooter();
-            });
-           
-        });
-
-
-    }
-
-});
-
-
